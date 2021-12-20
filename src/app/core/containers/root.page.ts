8e7744bbc9397c45cb6b1316941e694111900d9a
crislaez <crislaez30@gmail.com>
2021-12-20 14:50:38 +0100

    cambios color e imagenes

--- src/app/core/containers/root.page.ts
+++ src/app/core/containers/root.page.ts
@@ -11,12 +11,7 @@ import { Store } from '@ngrx/store';
    <!-- CABECERA  -->
    <ion-header no-border >
      <ion-toolbar mode="md|ios">
-       <ion-button fill="clear" size="small" slot="start" (click)="open()">
-         <!-- <ion-menu-button class="text-color"></ion-menu-button> -->
-       </ion-button>
        <ion-title class="text-color" >{{'COMMON.TITLE' | translate}}</ion-title>
-       <div size="small" slot="end">
-       </div>
      </ion-toolbar>
    </ion-header>
 
